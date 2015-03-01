92398ebbda995220beb1e91c297568e19b82ddca
Chris Ward <chriswarddev@gmail.com>
2015-03-01 21:51:05 +0000

    first test passes

--- TitleCase/lib/titlecase.rb
+++ TitleCase/lib/titlecase.rb
@@ -1,4 +1,8 @@
 class TitleCase
 
+  def title_case(title, minor_words='')
+    return title if title ==''
+    title.split(' ')
+  end
 
 end
--- TitleCase/spec/titlecase_spec.rb
+++ TitleCase/spec/titlecase_spec.rb
@@ -2,6 +2,10 @@ require 'titlecase'
 
 describe TitleCase do
 
+  tester = TitleCase.new
 
+  it 'should return an empty string when passed an empty string' do
+    expect(tester.title_case('')).to eq ''
+  end
 
 end

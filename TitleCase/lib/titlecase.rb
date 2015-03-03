29dc3a884b0f661f2da9560bdac4b6d4394ddab2
wardymate <chriswarddev@gmail.com>
2015-03-03 07:28:38 +0000

    titlecase solved

--- TitleCase/lib/titlecase.rb
+++ TitleCase/lib/titlecase.rb
@@ -2,8 +2,18 @@ class TitleCase
 
   def title_case(title, minor_words='')
     return title if title ==''
-    title_array = title.split(' ')
-    minor_words_array = minor_words.split(' ')
+    title_array = title.downcase.split(' ')
+    minor_words_array = minor_words.downcase.split(' ')
+    # title_array & minor_words_array
+    string = title_array.map do |word|
+      if minor_words_array.include?(word)
+        word.downcase
+      else
+        word.capitalize
+      end
+    end.join(' ')
+    string[0] = string[0].upcase
+    string
   end
 
 end
--- TitleCase/spec/titlecase_spec.rb
+++ TitleCase/spec/titlecase_spec.rb
@@ -12,5 +12,12 @@ describe TitleCase do
     expect(tester.title_case('a clash of KINGS', 'a an the of')).to eq 'A Clash of Kings'
   end
 
+  it 'should convert the string' do
+    expect(tester.title_case('THE WIND IN THE WILLOWS', 'The In')).to eq 'The Wind in the Willows'
+  end
+
+   it 'should convert the string' do
+    expect(tester.title_case('the quick brown fox')).to eq 'The Quick Brown Fox'
+  end
 
 end

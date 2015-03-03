73a5bd89ef1931aa67482bc025babee5b81961af
wardymate <chriswarddev@gmail.com>
2015-03-03 07:33:29 +0000

    titlecase method refactored

--- TitleCase/lib/titlecase.rb
+++ TitleCase/lib/titlecase.rb
@@ -2,16 +2,7 @@ class TitleCase
 
   def title_case(title, minor_words='')
     return title if title ==''
-    title_array = title.downcase.split(' ')
-    minor_words_array = minor_words.downcase.split(' ')
-    # title_array & minor_words_array
-    string = title_array.map do |word|
-      if minor_words_array.include?(word)
-        word.downcase
-      else
-        word.capitalize
-      end
-    end.join(' ')
+    string = title.downcase.split(' ').map { |word| minor_words.downcase.split(' ').include?(word) ? word.downcase : word.capitalize}.join(' ')
     string[0] = string[0].upcase
     string
   end

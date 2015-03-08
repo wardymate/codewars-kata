7a039ead50f2a1ffe9452d14514b015baa882f4b
wardymate <chriswarddev@gmail.com>
2015-03-08 21:39:48 +0000

    uniquer solved

--- uniqueinorder/lib/uniquer.rb
+++ uniqueinorder/lib/uniquer.rb
@@ -1,3 +1,11 @@
 class Uniquer
 
+  def unique_in_order(iterable)
+    if iterable.is_a? String
+      iterable.chars.reject.with_index{|e,i|iterable[i] == iterable[i-1]}
+    else
+      iterable.reject.with_index{|e,i|iterable[i] == iterable[i-1]}
+    end
+  end
+
 end
--- uniqueinorder/spec/uniquer_spec.rb
+++ uniqueinorder/spec/uniquer_spec.rb
@@ -2,6 +2,24 @@ require 'uniquer'
 
 describe Uniquer do
 
+  uniquer = Uniquer.new
 
+  it "should convert a string to an array" do
+    input = 'AAAABBBCCDAABBB'
+    output = ['A', 'B', 'C', 'D', 'A', 'B']
+    expect(uniquer.unique_in_order(input)).to eq (output)
+  end
+
+  it "should convert a string to an array" do
+    input = 'ABBCcAD'
+    output = ['A', 'B', 'C', 'c', 'A', 'D']
+    expect(uniquer.unique_in_order(input)).to eq (output)
+  end
+
+  it "should convert a string to an array" do
+    input = [1,2,2,3,3]
+    output = [1,2,3]
+    expect(uniquer.unique_in_order(input)).to eq (output)
+  end
 
 end

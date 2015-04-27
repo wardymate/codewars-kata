90543c54a0e78d2ae438892c27c3239f1935bba9
wardymate <chriswarddev@gmail.com>
2015-04-27 06:57:50 +0100

    search for letters solved

--- search_for_letters/lib/searcher.rb
+++ search_for_letters/lib/searcher.rb
@@ -1,6 +1,8 @@
 class Searcher
 
   def change(input_string)
-    "10000000000000000000000000"
+    output = ''
+    ('a'..'z').each {|letter| input_string.downcase.include?(letter) ? output << "1" : output << "0"}
+    output
   end
 end
--- search_for_letters/spec/searcher_spec.rb
+++ search_for_letters/spec/searcher_spec.rb
@@ -1,9 +1,15 @@
 require 'searcher'
 
 describe Searcher do
-
   it 'knows if the letter a is present in a string' do
-    expect(subject.change("a")).to eq "10000000000000000000000000"
+    expect(subject.change('a')).to eq "10000000000000000000000000"
   end
 
+  it 'knows if the letter a is NOt present in a string' do
+    expect(subject.change('')).to eq "00000000000000000000000000"
+  end
+
+  it 'knows that a b and z are in a string' do
+    expect(subject.change('a   **&  bZ')).to eq '11000000000000000000000001'
+  end
 end
